91ed4c17f6cf0aa46aaf920dd3f856c51de5fc47
Yana Tiulenina <122269490+YaTyulen@users.noreply.github.com>
2023-07-27 00:04:38 +0700

    добавление обработки отправки формы

--- js/form-work.js
+++ js/form-work.js
@@ -1,9 +1,16 @@
-import {removeClass, addClass, isEscapeKey} from './util.js';
+import {removeClass, addClass, isEscapeKey, showAlert} from './util.js';
 import {checkForm} from './form-validation.js';
 import {filterHandlers, defaultSettings} from './form-slider.js';
 import {controlBiggerHandler, controlSmallerHandler} from './form-scale.js';
+import {sendData} from './api.js';
+
+const SubmitButtonText = {
+  IDLE: 'Опубликовать',
+  SENDING: 'Публикую...'
+};
 
 const buttonImageLoading = document.querySelector('.img-upload__input'); //кнопка загрузки изображения
+const buttonSubmitNewImage = document.querySelector('.img-upload__submit');
 const buttonCloseWindowLoadingImage = document.querySelector('.img-upload__cancel'); //кнопка закрытия окна загрузки изображения
 const inputTextHashtags = document.querySelector('.text__hashtags'); //поле ввода хэштегов
 const textDescription = document.querySelector('.text__description'); //поле ввода описания фотографии (комментария)
@@ -14,11 +21,13 @@ const buttonScaleControlBigger = document.querySelector('.scale__control--bigger
 const imagePreview = document.querySelector('.img-upload__preview img');
 
 const radioButtonOriginal = document.querySelector('#effect-none');
+const form = document.querySelector('#upload-select-image');
 
 function windowNewImageHandlers(){
   buttonImageLoading.addEventListener('change', addImageHandler); //добавление обработчика клика по кнопке загрузки изображения
   buttonCloseWindowLoadingImage.addEventListener('click', closeWindowLoadingImageHandler); //добавдение обработчика клика по кнопке закрытия окна добавления изображения
-  inputTextHashtags.addEventListener('input', checkForm); //добавление обработчкика на проверку корректности заполнения формы
+
+  inputTextHashtags.addEventListener('input', buttonSubmitHandler); //добавление обработчика дизейбла кнопри при невалидной форме
 
   //удаление обработчика нажатия Esc при фокусе на полях ввода хэштега или комментария
   inputTextHashtags.addEventListener('focus', () => {
@@ -34,10 +43,53 @@ function windowNewImageHandlers(){
     document.addEventListener('keydown', onDocumentKeydown);
   });
 
-  buttonScaleControlSmaller.addEventListener('click', controlSmallerHandler);
-  buttonScaleControlBigger.addEventListener('click', controlBiggerHandler);
+  buttonScaleControlSmaller.addEventListener('click', controlSmallerHandler); //добавление обработчика уменьшения масштаба
+  buttonScaleControlBigger.addEventListener('click', controlBiggerHandler); //добавление обработчика увеличения масштаба
 
-  filterHandlers();
+  filterHandlers(); //обработчик добавления фильтов
+
+  setUserFormSubmit(closeWindowLoadingImageHandler); //обработчик отправки формы
+}
+
+//функция, обрабатывающая отправку формы
+function setUserFormSubmit(onSuccess) {
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const isValid = checkForm();
+    if (isValid) {
+      blockSubmitButton();
+      //document.removeEventListener('keydown', onDocumentKeydown);
+      sendData(new FormData(evt.target))
+        .then(onSuccess)
+        .catch((err) => {
+          showAlert(err.message);
+        })
+        .finally(unblockSubmitButton);
+    }
+  });
+}
+
+//проверить открытие окна сообщения
+//при открытии окна сообщения блокировать esc у окна новой фотки
+
+function blockSubmitButton() {
+  buttonSubmitNewImage.disabled = true;
+  buttonSubmitNewImage.textContent = SubmitButtonText.SENDING;
+}
+
+function unblockSubmitButton() {
+  buttonSubmitNewImage.disabled = false;
+  buttonSubmitNewImage.textContent = SubmitButtonText.IDLE;
+}
+
+//обработчик проверки формы перед отправкой
+function buttonSubmitHandler() {
+  if (checkForm()){
+    buttonSubmitNewImage.removeAttribute('disabled');
+  } else {
+    buttonSubmitNewImage.setAttribute('disabled', true);
+  }
 }
 
 //обработчик добавления изображения
@@ -73,4 +125,4 @@ function onDocumentKeydown(evt) {
   }
 }
 
-export {windowNewImageHandlers};
+export {windowNewImageHandlers, onDocumentKeydown};
