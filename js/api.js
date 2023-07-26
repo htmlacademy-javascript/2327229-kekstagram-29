90d9ca469d3e7d8f46eb611ee8e5aae243b68aed
Yana Tiulenina <122269490+YaTyulen@users.noreply.github.com>
2023-07-27 00:01:29 +0700

    добавление модуля по работе с сетью

---
+++ js/api.js
@@ -0,0 +1,44 @@
+import {showAlert} from './util.js';
+import {displaySuccessMessage, displayErrorMessage} from './message-boxes.js';
+
+const BASE_URL = 'https://29.javascript.pages.academy/kekstagram';
+const Route = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
+};
+
+const ErrorText = {
+  GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+};
+
+const getData = () => fetch(
+  `${BASE_URL}${Route.GET_DATA}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  })
+  .catch(() => {
+    throw new Error(showAlert(ErrorText.GET_DATA));
+  });
+
+
+const sendData = (body) => fetch(
+  `${BASE_URL}${Route.SEND_DATA}`,
+  {
+    method: 'POST',
+    body,
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error();
+    } else {
+      displaySuccessMessage();
+    }
+  })
+  .catch(() => {
+    throw displayErrorMessage();
+  });
+
+export {getData, sendData};
